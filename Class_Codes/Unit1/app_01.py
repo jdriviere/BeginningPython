99b8f6cf488bdf1794f97fda0ef951ae6d592b63
JD Riviere <tilithar@gmail.com>
2018-05-27 18:05:39 -0600

    Initial commit

---
+++ Class_Codes/Unit1/app_01.py
@@ -0,0 +1,11 @@
+# ============================================
+# @name BEGINNING PYTHON 3
+# @author J. Djimitry Riviere
+# @description Easy, simple introduction to the
+#              Python 3 programming language for
+#              those who want to start coding.
+# @date 05-25-2018
+#
+# MODULE 1
+# Class 01 - "Hello, World"
+# ============================================
