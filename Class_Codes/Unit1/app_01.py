2c24ef57e6e55cc7efabe3dd0a0622b945f8cac4
JD Riviere <tilithar@gmail.com>
2018-06-08 02:32:40 -0600

    More details on the assignment for app_01.py

--- Class_Codes/Unit1/app_01.py
+++ Class_Codes/Unit1/app_01.py
@@ -4,8 +4,40 @@
 # @description Easy, simple introduction to the
 #              Python 3 programming language for
 #              those who want to start coding.
-# @date 05-25-2018
+# @date 06-08-2018
 #
 # UNIT 1
 # Activity 01 - "Hello, World"
+#
+# NOTE:
+# You don't have to change anything from the
+# code below. This is an example I left for you
+# to evaluate and use as a reference.
+#
+# SIDENOTE:
+# In comparison to a lot of programming languages,
+# printing a string into the Command Line is
+# very easy and simple, as you see below. If you were
+# to look at other languages, such as Java or C/C++,
+# you would see that it is much more complex, as it
+# requires more commands to make a simple line print.
 # ============================================
+print("Hello, World!")
+
+# --------------------------------------------#
+# ASSIGNMENT:
+# Print sentence you want into the Command Line.
+# Make sure you run the command in your Terminal
+# to make sure your code works!
+# Once your done, and if you have set up a Github
+# account, make sure you upload your code to your
+# repository!
+#
+# OBJECTIVE:
+# The goal of this assignment is to teach you
+# how to print a string (we will see what 'strings'
+# are in the next Unit) into your Command Line (or
+# Terminal).
+#
+# --> START YOUR ASSIGNMENT BELOW! <--
+# --------------------------------------------
