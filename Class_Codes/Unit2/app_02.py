1ecadcc1e842f46c38854dff14ece13b30c1875c
JD Riviere <tilithar@gmail.com>
2018-06-13 01:31:52 -0600

    Unit 2 assignment ready. v1.0

--- Class_Codes/Unit2/app_02.py
+++ Class_Codes/Unit2/app_02.py
@@ -4,20 +4,46 @@
 # @description Easy, simple introduction to the
 #              Python 3 programming language for
 #              those who want to start coding.
-# @date 05-25-2018
+# @date 06-11-2018
 #
 # UNIT 2
 # Activity 01 - Data Types
+#
+# NOTE:
+# You don't have to change anything from the
+# code below. This is an example I left for you
+# to evaluate and use as a reference.
+#
+# SIDENOTE:
+# The advantage of Python variables is that you
+# don't have to declare its type, as you would
+# do with other more complex programming languages.
+# For instance, let's compare Java and Python:
+# 
+# --- JAVA ---  |  --- PYTHON ---
+# int a = 30;   |  a = 30
+#
+# *Which one would you rather choose? ;)
 # ============================================
+a = 30
+b = "Hello"
+c = 3.5
+d = True
 
+# The method type(value) allows you to determine what kind of data type
+# a variable is.
+print("Value of a:", a, " - Type of value a:", type(a))
+print("Value of b:", b, " - Type of value b:", type(b))
+print("Value of c:", c, " - Type of value c:", type(c))
+print("Value of d:", d, " - Type of value d:", type(d))
 
-# ============================================
-# UNIT 2
-# Activity 02 - ???
-# ============================================
-
-
-# ============================================
-# UNIT 2
-# Activity 03 - ???
-# ============================================
+# --------------------------------------------#
+# ASSIGNMENT:
+# Create as many variables as you want and give
+# them each a value. Then determine the type of
+# each value, using the method type().
+#
+# OBJECTIVE:
+#
+# --> START YOUR ASSIGNMENT BELOW! <--
+# --------------------------------------------
