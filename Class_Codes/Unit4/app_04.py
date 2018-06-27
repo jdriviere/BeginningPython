f571e76b76f29a57da811280e7073a2dbbac0d43
JD Riviere <tilithar@gmail.com>
2018-06-27 17:26:36 -0600

    Added Unit 4

---
+++ Class_Codes/Unit4/app_04.py
@@ -0,0 +1,30 @@
+# ============================================
+# @name BEGINNING PYTHON 3
+# @author J. Djimitry Riviere
+# @description Easy, simple introduction to the
+#              Python 3 programming language for
+#              those who want to start coding.
+# @date 06-27-2018
+#
+# UNIT 4
+# Activity 01 - Data Types (Part 2)
+#
+# NOTE:
+# You don't have to change anything from the
+# code below. This is an example I left for you
+# to evaluate and use as a reference.
+#
+# SIDENOTE:
+#
+# ============================================
+pass
+
+# --------------------------------------------#
+# ASSIGNMENT:
+# 
+#
+# OBJECTIVE:
+# 
+#
+# --> START YOUR ASSIGNMENT BELOW! <--
+# --------------------------------------------
