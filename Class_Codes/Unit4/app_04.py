b010bc749639fcca84b83ea1136ffa82ce8ad5b3
JD Riviere <tilithar@gmail.com>
2018-06-28 00:57:53 -0600

    App 04

--- Class_Codes/Unit4/app_04.py
+++ Class_Codes/Unit4/app_04.py
@@ -7,7 +7,7 @@
 # @date 06-27-2018
 #
 # UNIT 4
-# Activity 01 - Data Types (Part 2)
+# Activity 01 - Arithmetics and Operators
 #
 # NOTE:
 # You don't have to change anything from the
@@ -15,16 +15,86 @@
 # to evaluate and use as a reference.
 #
 # SIDENOTE:
+# The operators seen in this Unit are very similar
+# to most programming languages out there, for the
+# most part. Let's compare Python with Java:
+#
+# --- JAVA ---                |   --- PYTHON ---
+# int a = 3;                  |   a = 3
+# int b = 2;                  |   b = 2
+# system.out.print(a < b);    |   print(a < b)
+# system.out.print(a > b);    |   print(a > b)
+# system.out.print(a <= b);   |   print(a <= b)
+# system.out.print(a >= b);   |   print(a >= b)
+# system.out.print(a == b);   |   print(a == b)
+# system.out.print(a != b);   |   print(a != b)
+#
+# Notice how the Comparison Operators (above) are very similar
+# to that of Java. In truth, most types of operators
+# found in the Python language are very similar to
+# those of Java. Let's try more operators examples:
+#
+# --- JAVA ---                              |   --- PYTHON ---
+# int a = 3;                                |   a = 3
+# int b = 2;                                |   b = 2
+# system.out.print(a + b);                  |   print(a + b)
+# system.out.print(a - b);                  |   print(a - b)
+# system.out.print(a * b);                  |   print(a * b)
+# system.out.print(a / b);                  |   print(a / b)
+# system.out.print(a % b);                  |   print(a % b)
+# system.out.print(Math.pow(a, b));         |   print(a ** b)
+# system.out.print(Math.floorDiv(a, b));    |   print(a // b)
+#
+# When it comes to Arithmetic Operators, as seen above, Python
+# trumps over Java, when it comes to simplicity, and it is much
+# easier to remember the last two operators (especially),
+# compared to that of Java. Let's compare another type of
+# operators: the Logical Operators.
+#
+# --- JAVA ---                                |   --- PYTHON ---
+# int a = 3;                                  |   a = 3
+# int b = 2;                                  |   b = 2
+# system.out.print(a == 2 && b == 3);         |   print(a == 2 and b == 3)
+# system.out.print(a == 2 || b == 3);         |   print(a == 2 or b == 3)
+# system.out.print( !(a == 2 && b == 3) );    |   print( not(a == 2 and b == 3) )
+#
+# In these last examples, you can see that Python uses words
+# instead of symbols, when it comes to logic; but other than
+# that, the differences are pretty slim. Now, below, I have
+# written some examples of each and every Operators we've
+# covered in the videos. There are many more operators in the
+# Python language, but I have kept only the simplest ones
+# for the class. ;)
 #
 # ============================================
 pass
 
 # --------------------------------------------#
 # ASSIGNMENT:
-# 
+# Come up with a few arithmetic operations, logical
+# operations, and comparisons of your own, following
+# the model provided above. Print the results of each
+# operations you compute.
+# Create one or two lists and/or tuples, then use the
+# Membership Operators ('in' and 'not in') to verify
+# if a value is included or not.
+# Create two or three variables, then use the id() function
+# to determine these variables' id (memory location) and
+# print them. Then use the Identity Operators ('is' and 'is not')
+# to determine if these variables are the same (have the same ID),
+# then print the results.
+#
+# NOTE: Don't forget to upload your code into your Github repository
+#       (if you have created an account, or already have an account)
+#       once you are done! :)
 #
 # OBJECTIVE:
-# 
+# Operators are among the most important aspects of
+# any programming languages. Understanding how to use
+# them efficiently will help you majorly in writing and
+# understanding coding. Actually, most complex subjects
+# of Python (or any other languages) function because
+# of these Operators.
 #
 # --> START YOUR ASSIGNMENT BELOW! <--
 # --------------------------------------------
